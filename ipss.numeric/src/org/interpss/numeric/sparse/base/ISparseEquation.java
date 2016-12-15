92a5f9b67026113faecac5fae012dc0729ba8dae
jinjingfarm <mzhou@interpss.org>
2016-12-15 08:34:38 +0800

    removed unused SparseEqn classes

--- ipss.numeric/src/org/interpss/numeric/sparse/base/ISparseEquation.java
+++ ipss.numeric/src/org/interpss/numeric/sparse/base/ISparseEquation.java
@@ -30,63 +30,24 @@ import org.interpss.numeric.exp.IpssNumericException;
  * 
  */
 public interface ISparseEquation {
+	public static enum IndexType { Row, Col };
+	
+	///////////////////////////////////////////////////////////////
+	//////////// Square and Non-Square Matrix Function /////////////
+
 	/**
-	 * sparse eqn solver type
+	 * Check if the matrix is square
 	 * 
-	 * @author mzhou
-	 *
+	 * @return true if it is a square matrix
 	 */
-	public static enum SolverType {Default, Native};
-	
-	/**
-	 * 	get matrix dimension.
-     * 
-     * @return the dimension
-     */
-	int getDimension();
+	boolean isSquareMatrix();
 
-	/**
-	 * 	Set matrix dimension. The index number is from 0 to n-1
-     * 
-     * @param n
-     */
-	void setDimension( final int n );
-
-	/**
-	 * Increase matrix dimension by 1. B[n] = 0.0, aii(n)=1.0, aij(n)=0.0 
-     * 
-     * @param n
-     */
-	void increaseDimension();
-	
 	/**
 	 * Get the total elements in the matrix
 	 * 
 	 * @return the total elements
 	 */
 	int getTotalElements();	
-	
-	/**
-	 * LU decomposition of the matrix.
-	 * 
-	 * @param tolerance the tolerance for matrix singular detection
-	 * @return if succeed return true.
-	 */
-	boolean luMatrix( final double tolerance) throws IpssNumericException;
-		
-	/**
-	 * LU decomposition of the matrix and the solve the [A]X = B problem.
-	 * 
-	 * @param tolerance the tolerance for matrix singular detection
-	 * @return if succeed return true.
-	 */
-	boolean luMatrixAndSolveEqn( final double tolerance)  throws IpssNumericException;
-		
-	/**
-	 * Solve the [A]X = B problem
-	 * 
-	 */
-	void solveEqn() throws IpssNumericException;			
 
 	/**
 	 * Set all b elements to 0.0 and bi = 1.0, a unity vector.
@@ -113,15 +74,7 @@ public interface ISparseEquation {
 	 * 
 	 */
 	void setToZero();
-	
-	/**
-	 * During the LU process, aii might be zero. An exception will be thrown. This function
-	 * is for getting zero aii row number for error reporting
-	 * 
-	 * @return the row number
-	 */
-	int getZeroAii_row();
-	
+
 	/**
 	 * Flag to indicate if the matrix is factorized
 	 * 
@@ -135,6 +88,60 @@ public interface ISparseEquation {
 	 * @param isLUFac the flag
 	 */
 	void setFactorized(boolean isLUFac);
+	
+	///////////////////////////////////////////////////////////////
+	//////////// Square Matrix Only Function //////////////////////
+	
+	/**
+	 * 	get matrix dimension.
+     * 
+     * @return the dimension
+     */
+	int getDimension();
+
+	/**
+	 * 	Set matrix dimension. The index number is from 0 to n-1
+     * 
+     * @param n
+     */
+	//void setDimension( final int n );
+
+	/**
+	 * Increase matrix dimension by 1. B[n] = 0.0, aii(n)=1.0, aij(n)=0.0 
+     * 
+     * @param n
+     */
+	void increaseDimension();
+
+	/**
+	 * LU decomposition of the matrix.
+	 * 
+	 * @param tolerance the tolerance for matrix singular detection
+	 * @return if succeed return true.
+	 */
+	boolean luMatrix( final double tolerance) throws IpssNumericException;
+		
+	/**
+	 * LU decomposition of the matrix and the solve the [A]X = B problem.
+	 * 
+	 * @param tolerance the tolerance for matrix singular detection
+	 * @return if succeed return true.
+	 */
+	boolean luMatrixAndSolveEqn( final double tolerance)  throws IpssNumericException;
+		
+	/**
+	 * Solve the [A]X = B problem
+	 * 
+	 */
+	void solveEqn() throws IpssNumericException;			
+	
+	/**
+	 * During the LU process, aii might be zero. An exception will be thrown. This function
+	 * is for getting zero aii row number for error reporting
+	 * 
+	 * @return the row number
+	 */
+	int getZeroAii_row();
 	
 	/**
 	 * get cached bus id by the row number
@@ -151,4 +158,22 @@ public interface ISparseEquation {
 	 * @param id bus id
 	 */
 	void setBusId(int row, String id);
+	
+	///////////////////////////////////////////////////////////////
+	//////////// Non-Square Matrix Only Function //////////////////
+
+	/**
+	 * 	get matrix dimension.
+     * 
+     * @return the dimension
+     */
+	int getDimension(IndexType type);
+
+	/**
+	 * 	Set matrix dimension. The index number is from 0 to n-1
+     * 
+     * @param row
+     * @param col
+     */
+	//void setDimension( final int row, final int col );	
 }	
