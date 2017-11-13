c97972f3a2329fa22579cf400d0a1a7c40fe5d21
thuang <eetonyhuang@gmail.com>
2017-11-13 11:46:53 -0800

    update the complex3x1

--- ipss.numeric/src/org/interpss/numeric/datatype/Complex3x1.java
+++ ipss.numeric/src/org/interpss/numeric/datatype/Complex3x1.java
@@ -199,6 +199,17 @@ public class Complex3x1 implements java.io.Serializable {
 	 * @return
 	 *
 	*/
+	public Complex dotProduct(Complex3x1 y) {
+		return this.a_0.multiply(y.a_0).add(this.b_1.multiply(y.b_1)).add(this.c_2.multiply(y.c_2));
+	}
+	
+	/**
+	 * treat complex3x1 objects as vectors, dot product returns the inner product of the two vectors, return a Complex value
+	 * 
+	 * @param y the complex3x1 value
+	 * @return
+	 *
+	*/
 	public Complex3x1 multiply(Complex3x1 y) {
 		return new Complex3x1(this.a_0.multiply(y.a_0),this.b_1.multiply(y.b_1),this.c_2.multiply(y.c_2));
 	}
