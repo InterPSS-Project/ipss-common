6e49176e48ac3cdb786b12f6011c4f1cadc18699
mike <mike.zhou@interpss.org>
2020-07-28 11:03:04 +0800

    added a new JavaOO test file

---
+++ ipss.symnum/src/JavaOOTest.java
@@ -0,0 +1,27 @@
+import java.math.*;
+import java.util.*;
+
+public class JavaOOTest {
+    public static void main(String[] args) {
+        BigInteger a = BigInteger.valueOf(1), // without OO
+                b = 2, // with OO
+
+                c1 = a.negate().add(b.multiply(b)).add(b.divide(a)), // without OO
+                c2 = -a + b*b + b/a; // with OO
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("-a + b*b + b/a = " + c2);
+
+        if (c1.compareTo(c2)<0 || c1.compareTo(c2)>0) // without OO
+            System.out.println("impossible");
+        if (c1<c2 || c1>c2) // with OO
+            System.out.println("impossible");
+
+        HashMap<String, String> map = new HashMap<>();
+        if (!map.containsKey("qwe"))
+            map.put("qwe", map.get("asd")); // without OO
+        if (map["qwe"]==null)
+            map["qwe"] = map["asd"]; // with OO
+    }
+}
