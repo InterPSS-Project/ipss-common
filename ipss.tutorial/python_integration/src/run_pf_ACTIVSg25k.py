781a602da305a0dcc5e9eb61be00e0e1d0eb585a
ipssdev3 <scutparis@gmail.com>
2026-02-02 11:19:26 -0700

    add acknowlegement of the test case sources

--- ipss.tutorial/python_integration/src/run_pf_ACTIVSg25k.py
+++ ipss.tutorial/python_integration/src/run_pf_ACTIVSg25k.py
@@ -1,3 +1,14 @@
+##
+# Acknowledgment:
+# The synthetic 25k-bus electric grid test case used in this example is provided by Texas A&M University’s energy and power group researchers.
+# https://electricgrids.engr.tamu.edu/
+#  
+# For details of the synthetic grid, please visit the website above and/or refer to the following references:
+#  [1] A. B. Birchfield; T. Xu; K. M. Gegner; K. S. Shetye; T. J. Overbye, “Grid Structural Characteristics as Validation Criteria for Synthetic Networks,”  in IEEE Transactions on Power Systems, vol. 32, no. 4, pp. 3258-3265, July 2017.
+#  [2] A. B. Birchfield; K. M. Gegner; T. Xu; K. S. Shetye; T. J. Overbye, “Statistical Considerations in the Creation of Realistic Synthetic PowerGrids for Geomagnetic Disturbance Studies,” in IEEE Transactions on Power Systems, vol. 32, no. 2, pp. 1502-1510, March 2017.
+#  [3] K. M. Gegner; A. B. Birchfield; T. Xu; K. S. Shetye; T. J. Overbye, “A methodology for the creation of geographically realistic synthetic powerflow models,” 2016 IEEE Power and Energy Conference at Illinois (PECI), Urbana, IL, 2016, pp. 1-6.
+#
+
 import jpype
 import jpype.imports
 from jpype.types import *
